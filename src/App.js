f2e200eeaa638f4c66d816eb313a0733a55f5b56
AncherHisogi <anchertunchar@gmail.com>
2021-01-21 16:38:37 +0200

    added new component and trying it out

--- src/App.js
+++ src/App.js
@@ -1,8 +1,10 @@
+import React from 'react';
 import logo from './logo.svg';
 import './App.css';
 
-function App() {
-  return (
+class App extends React.Component {
+  render () {
+   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
@@ -12,7 +14,10 @@ function App() {
         
       </header>
     </div>
-  );
+   )
+  }
+     
+   
 }
 
 export default App;
