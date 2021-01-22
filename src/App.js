314f64a14a039342ebe5559ab31003dd757224ab
AncherHisogi <anchertunchar@gmail.com>
2021-01-22 16:02:19 +0200

    added json placeholder rest api for user entries and wrapped the users in a new scroll component

--- src/App.js
+++ src/App.js
@@ -1,22 +1,29 @@
 import React from 'react';
 import CardList from './CardList';
-import {robots} from './robots';
 import SearchBox from './SearchBox';
-
+import Scroll from './Scroll';
 
 class App extends React.Component{
     constructor(){
         super()
         this.state ={
-            robots: robots,
+            robots: [],
             searchfield: ''
         } 
     }
+
+    componentDidMount(){
+        fetch('https://jsonplaceholder.typicode.com/users')
+            .then(response => response.json())
+            .then(users => this.setState({robots:users}));
+    }
+
 
     onSearchChange = (event) => {
         this.setState({ searchfield:event.target.value })
     }
 
+    
     render(){
         const filteredRobots = this.state.robots.filter(robot =>{
             return robot.name.toLowerCase().includes(this.state.searchfield.toLowerCase());
@@ -26,7 +33,9 @@ class App extends React.Component{
             <div className='tc'>
                 <h1>Friends</h1>
                 <SearchBox searchChange={this.onSearchChange}/>
-                <CardList robots={filteredRobots}/>
+                <Scroll>
+                    <CardList robots={filteredRobots}/>
+                </Scroll>
             </div>
         );
     }   
